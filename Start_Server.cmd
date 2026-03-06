e4af4241fc4be9ceb33410922e3352d79f0bea31
Jomitm <mathew.jomit@gmail.com>
2026-03-06 11:33:15 +0530

    Add simple start server cmd

---
+++ Start_Server.cmd
@@ -0,0 +1,9 @@
+@echo off
+echo ==============================================
+echo CRWI Symposium 2026 - Local Development Server
+echo ==============================================
+echo.
+echo Starting the local Node.js server...
+echo.
+npm start
+pause
